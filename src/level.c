0086d80ce527a5ce1ea4fdafe7b7aa7d5407e300
Luca-coder07 <lucarandrianirina2507@gmail.com>
2026-03-27 00:53:08 +0300

    [UPDATE] Add level architecture

--- src/level.c
+++ src/level.c
@@ -15,6 +15,8 @@ void UpdateLevel(t_level *level, float dt)
 void DrawLevel(t_level *level)
 {
     (void)level;
+    DrawRectangle(0, screen_height * 0.9, screen_width, screen_height, GRAY);
+    DrawLineEx((Vector2){0, screen_height * 0.9}, (Vector2){screen_width, screen_height * 0.9}, 4, BLACK);
 }
 
 void UnloadLevel(t_level *level)
